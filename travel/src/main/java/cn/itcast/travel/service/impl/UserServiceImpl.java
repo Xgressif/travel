c71121204c4b47ef71b7ce13e64fa52dcd603cff
Xgressif <xyw19961205@163.com>
2019-05-12 16:17:20 +0800

    travel

---
+++ travel/src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.UserService;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public boolean regist(User user) {
+        return false;
+    }
+}
